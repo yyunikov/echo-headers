66d2886d19cc684c24deeaa601f94fd175559c1a
yyunikov <yyunikov@gmail.com>
2017-10-06 18:39:48 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/HeadersController.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+public class HeadersController {
+
+  @RequestMapping("/request-headers")
+  public Map<String, String> get(final HttpServletRequest request, final HttpServletResponse response) {
+    final Enumeration<String> headerNames = request.getHeaderNames();
+    final Map<String, String> headers = new HashMap<>();
+    while (headerNames.hasMoreElements()) {
+      final String headerName = headerNames.nextElement();
+      final String headerValue = response.getHeader(headerName);
+      headers.put(headerName, headerValue);
+    }
+
+    return headers;
+  }
+
+}
+
